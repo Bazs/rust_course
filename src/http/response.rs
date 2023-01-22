f704ecbffbdd4220e3ddc831ad0a95bcda564cb8
Istvan Balazs Opra <opra.balazs@gmail.com>
2023-01-22 18:20:43 +0900

    Avoid copying the body string in Response
    Send reponses for all cases in the server

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,3 +1,6 @@
+use std::io::{Result as IoResult, Write};
+use std::net::TcpStream;
+
 use super::StatusCode;
 
 pub struct Response {
@@ -8,5 +11,19 @@ pub struct Response {
 impl Response {
     pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
         Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+        let body = match &self.body {
+            Some(s) => s,
+            None => "",
+        };
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body
+        )
     }
 }
