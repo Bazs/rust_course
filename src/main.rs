53f0c27088bcaff8b8754cd9fb6514126bda5194
Istvan Balazs Opra <opra.balazs@gmail.com>
2023-01-24 21:15:06 +0900

    Add request handler trait and implementation
    Allow any struct implementing Write in Response::send

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 use http::Method;
 use server::Server;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
     let get = Method::GET;
@@ -12,5 +14,5 @@ fn main() {
     let post = Method::POST;
 
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    server.run(WebsiteHandler);
 }
